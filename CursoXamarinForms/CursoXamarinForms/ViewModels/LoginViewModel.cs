7baa724347b368e78ad1471c903417e6596e874b
AXEL FRIAS HERNANDEZ <axel.frias257@gmail.com>
2020-03-12 13:36:25 -0600

    BaseViewModel

--- CursoXamarinForms/CursoXamarinForms/ViewModels/LoginViewModel.cs
+++ CursoXamarinForms/CursoXamarinForms/ViewModels/LoginViewModel.cs
@@ -1,26 +1,99 @@
 ﻿namespace CursoXamarinForms.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Atributos
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
         public bool IsRemembered { get; set; }
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
-        
+
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debes ingresar tu correo",
+                    "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Debes ingresar tu contraseña",
+                    "Aceptar");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email == "suripanta@gmail.com" && this.Password == "hola")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "El Dios Suripanta ha vuelto",
+                    "Aceptar");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "No mames",
+                    "Teclea bien, perro",
+                    "Alv");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+        }
+
         public ICommand RegisterCommand { get; set; }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
+            this.IsEnabled = true;
             this.IsRemembered = true;
-            this.Email = "Hola";
         }
         #endregion
     }
